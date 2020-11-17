6c80a01183b9d75610edfbd667ce4173fe21ce92
fbosi <bosisoftwareengineering@protonmail.com>
2020-11-16 22:53:02 -0300

    Feat: Add Enemy class

--- src/main/java/basicTextGame/Main.java
+++ src/main/java/basicTextGame/Main.java
@@ -4,97 +4,93 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        Random rand = new Random();
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    Random rand = new Random();
 
-        // Game variables
-        String[] enemies = { "Skeleton", "Zombie", "Warrior", "Assassin" };
-        int maxEnemyHealth = 75;
-        int enemyAttackDamage = 25;
-        int healthPotionHealAmount = 30;
-        int healthPotionDropChance = 50; // percentage
+    // Game variables
+    String[] enemies = { "Skeleton", "Zombie", "Warrior", "Assassin" };
+    int healthPotionHealAmount = 30;
 
-        // Player
-        Player player = new Player();
+    // Player
+    Player player = new Player();
 
-        boolean running = true;
+    boolean running = true;
 
-        System.out.println("Welcome to the Dungeon!");
+    System.out.println("Welcome to the Dungeon!");
 
-        GAME: while (running) {
-            System.out.println("_____________________________________________");
+    GAME: while (running) {
+      System.out.println("_____________________________________________");
 
-            int enemyHealth = rand.nextInt(maxEnemyHealth);
-            String enemy = enemies[rand.nextInt(enemies.length)];
-            System.out.println("\t# " + enemy + " appeared! #\n");
+      Enemy enemy = new Enemy(enemies[rand.nextInt(enemies.length)]);
+      System.out.println("\t# " + enemy.getName() + " appeared! #\n");
 
-            while (enemyHealth > 0) {
-                System.out.println("\tYour HP: " + player.getHealth());
-                System.out.println("\t" + enemy + "'s HP: " + enemyHealth);
-                System.out.println("\n\tWhat would you like to do?");
-                System.out.println("\t1. Attack");
-                System.out.println("\t2. Drink health potion");
-                System.out.println("\t3. Run!");
+      while (enemy.getHealth() > 0) {
+        System.out.println("\tYour HP: " + player.getHealth());
+        System.out.println("\t" + enemy.getName() + "'s HP: " + enemy.getHealth());
+        System.out.println("\n\tWhat would you like to do?");
+        System.out.println("\t1. Attack");
+        System.out.println("\t2. Drink health potion");
+        System.out.println("\t3. Run!");
 
-                String input = in.nextLine();
-                if (input.equals("1")) {
-                    int damageDealt = player.dealDamage();
-                    int damageTaken = rand.nextInt(enemyAttackDamage);
+        String input = in.nextLine();
+        if (input.equals("1")) {
+          int damageDealt = player.dealDamage();
+          int damageTaken = enemy.dealDamage();
 
-                    enemyHealth -= damageDealt;
-                    player.receiveDamage(damageTaken);
+          enemy.receiveDamage(damageDealt);
+          player.receiveDamage(damageTaken);
 
-                    System.out.println("\t> You strike the " + enemy + " for " + damageDealt + " damage.");
-                    System.out.println("\t> You received " + damageTaken + " in retaliation!");
+          System.out.println("\t> You strike the " + enemy.getName() + " for " + damageDealt + " damage.");
+          System.out.println("\t> You received " + damageTaken + " in retaliation!");
 
-                    if (player.getHealth() < 1) {
-                        System.out.println("\t> You have taken too much damage, you are too weak to continue!");
-                        break;
-                    }
-                } else if (input.equals("2")) {
-                    player.drinkPotion(healthPotionHealAmount);
-                } else if (input.equals("3")) {
-                    System.out.println("\tYou run away from the " + enemy + "!");
-                    continue GAME;
-                } else {
-                    System.out.println("Invalid command!");
-                }
-            }
-
-            if (player.getHealth() < 1) {
-                System.out.println("You limp out of the dungeon, weak from the battle.");
-                break;
-            }
-
-            System.out.println("_____________________________________________");
-            System.out.println(" # " + enemy + " was defeated! #");
-            System.out.println(" # You have " + player.getHealth() + " HP left. #");
-            if (rand.nextInt(100) < healthPotionDropChance) {
-                player.setHealthPotions(1);
-            }
-            System.out.println("_____________________________________________");
-            System.out.println("What would you like to do now?");
-            System.out.println("1. Continue fighting");
-            System.out.println("2. Exit the dungeon");
-
-            String input = in.nextLine();
-
-            while (!input.equals("1") && !input.equals("2")) {
-                System.out.println("Invalid command!");
-                input = in.nextLine();
-            }
-
-            if (input.equals("1")) {
-                System.out.println("You continue on your adventure!");
-            } else if (input.equals("2")) {
-                System.out.println("You exit the dungeon, successful from your adventure!");
-                break;
-            }
+          if (player.getHealth() < 1) {
+            System.out.println("\t> You have taken too much damage, you are too weak to continue!");
+            break;
+          }
+        } else if (input.equals("2")) {
+          player.drinkPotion(healthPotionHealAmount);
+        } else if (input.equals("3")) {
+          System.out.println("\tYou run away from the " + enemy.getName() + "!");
+          continue GAME;
+        } else {
+          System.out.println("Invalid command!");
         }
+      }
 
-        System.out.println("\n###################");
-        System.out.println("Thanks for playing!");
-        System.out.println("###################");
+      if (player.getHealth() < 1) {
+        System.out.println("You limp out of the dungeon, weak from the battle.");
+        break;
+      }
+
+      System.out.println("_____________________________________________");
+      System.out.println(" # " + enemy.getName() + " was defeated! #");
+      System.out.println(" # You have " + player.getHealth() + " HP left. #");
+      if (rand.nextInt(100) < enemy.getHealthPotionDropChance()) {
+        player.setHealthPotions(1);
+      }
+      System.out.println("_____________________________________________");
+      System.out.println("What would you like to do now?");
+      System.out.println("1. Continue fighting");
+      System.out.println("2. Exit the dungeon");
+
+      String input = in.nextLine();
+
+      while (!input.equals("1") && !input.equals("2")) {
+        System.out.println("Invalid command!");
+        input = in.nextLine();
+      }
+
+      if (input.equals("1")) {
+        System.out.println("You continue on your adventure!");
+      } else if (input.equals("2")) {
+        System.out.println("You exit the dungeon, successful from your adventure!");
+        break;
+      }
     }
+
+    System.out.println("\n###################");
+    System.out.println("Thanks for playing!");
+    System.out.println("###################");
+  }
 }
--- src/main/java/basicTextGame/Player.java
+++ src/main/java/basicTextGame/Player.java
@@ -3,67 +3,64 @@ package basicTextGame;
 import java.util.Random;
 
 public class Player {
-    private int health;
-    private int maxHealth;
-    private int attackDamage;
-    private int numHealthPots;
-    Random rand = new Random();
+  private int health;
+  private int maxHealth;
+  private int attackDamage;
+  private int numHealthPots;
+  Random rand = new Random();
 
-    public Player() {
-        this.health = 100;
-        this.maxHealth = 100;
-        this.attackDamage = 50;
-        this.numHealthPots = 3;
-    }
+  public Player() {
+    this.health = 100;
+    this.maxHealth = 100;
+    this.attackDamage = 50;
+    this.numHealthPots = 3;
+  }
+  public Player(int health, int attackDamage, int numHealthPots) {
+    this.health = health;
+    this.maxHealth = health;
+    this.attackDamage = attackDamage;
+    this.numHealthPots = numHealthPots;
+  }
 
-    public Player(int health, int attackDamage, int numHealthPots) {
-        this.health = health;
-        this.maxHealth = health;
-        this.attackDamage = attackDamage;
-        this.numHealthPots = numHealthPots;
-    }
+  public int getHealth() {
+    return this.health;
+  }
 
-    public int getHealth() {
-        return this.health;
+  public void setHealth(int value) {
+    if (value <= this.maxHealth) {
+      this.health = value;
+    } else {
+      this.health = this.maxHealth;
     }
+  }
 
-    public void setHealth(int value) {
-        if (value <= this.maxHealth) {
-            this.health = value;
-        } else {
-            this.health = this.maxHealth;
-        }
+  public void setHealthPotions(int value) {
+    this.numHealthPots += value;
+    if (value == 1) {
+      System.out.println("# You have found a potion");
+      System.out.println("# You have found a potion");
+    } else {
+      System.out.println("# You have found " + value + " potions");
     }
+    System.out.println(" # You now have " + this.numHealthPots + " health potion(s). #");
+  }
 
-    public void setHealthPotions(int value) {
-        this.numHealthPots += value;
-        if (value == 1) {
-            System.out.println("# You have found a potion");
-            System.out.println("# You have found a potion");
-        } else {
-            System.out.println("# You have found " + value + " potions");
-        }
-        System.out.println(" # You now have " + this.numHealthPots + " health potion(s). #");
-    }
+  public int dealDamage() {
+    return rand.nextInt(this.attackDamage);
+  }
+  public int receiveDamage(int damage) {
+    this.setHealth(this.health - damage);
+    return this.health;
+  }
 
-    public int dealDamage() {
-        return rand.nextInt(this.attackDamage);
+  public void drinkPotion(int cure) {
+    if (this.numHealthPots > 0) {
+      this.setHealth(this.health + cure);
+      this.numHealthPots--;
+      System.out.println("\t> You drink a health potion, healing yourself for " + cure + "." + "\n\t> You have now "
+          + this.health + " HP." + "\n\t> You have " + this.numHealthPots + " health potions left.\n");
+    } else {
+      System.out.println("You have no health potions");
     }
-
-    public int receiveDamage(int damage) {
-        this.setHealth(this.health - damage);
-        return this.health;
-    }
-
-    public void drinkPotion(int cure) {
-        if (this.numHealthPots > 0) {
-            this.setHealth(this.health + cure);
-            this.numHealthPots--;
-            System.out.println("\t> You drink a health potion, healing yourself for " + cure + "."
-                    + "\n\t> You have now " + this.health + " HP." + "\n\t> You have " + this.numHealthPots
-                    + " health potions left.\n");
-        } else {
-            System.out.println("You have no health potions");
-        }
-    }
+  }
 }
